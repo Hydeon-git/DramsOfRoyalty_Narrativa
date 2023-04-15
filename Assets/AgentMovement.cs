0ca9233a3faf20f7945f8769ad7ed8a35f2d0720
senc1yo <jormateo8@gmail.com>
2023-04-15 12:26:45 +0200

    ahora pueden andar los soldados
    habrá que hacer que se queden quietos cuando hablen, poco a poco
    
    ah y animarle jeje

---
+++ Assets/AgentMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMovement : MonoBehaviour
+{
+    public float roamRadius = 10.0f; // radio de movimiento aleatorio
+    public float roamTimer = 5.0f; // tiempo máximo de espera en cada punto
+    private float timer;
+
+    private NavMeshAgent navAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        timer = roamTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // Si el agente ha llegado a su destino o ha pasado demasiado tiempo en un punto, asigna un nuevo destino
+        if (navAgent.remainingDistance <= navAgent.stoppingDistance || timer >= roamTimer)
+        {
+            SetNewRandomDestination();
+        }
+    }
+
+    void SetNewRandomDestination()
+    {
+        // Genera un punto aleatorio dentro del radio de movimiento y lo asigna como el nuevo destino
+        Vector3 randomDirection = Random.insideUnitSphere * roamRadius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, roamRadius, 1);
+        Vector3 finalPosition = hit.position;
+
+        navAgent.SetDestination(finalPosition);
+
+        timer = 0;
+    }
+}
